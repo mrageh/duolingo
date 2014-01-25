69f72223756104d103b6d580685d09bafa7b30e5
Adam89 <Adamm1@aston.ac.uk>
2014-01-25 00:48:02 -0700

    languages studied and current languages can now be found out for a user

--- lib/duolingo/user.rb
+++ lib/duolingo/user.rb
@@ -1,16 +1,33 @@
 module Duolingo
   class User
-    attr_reader :response
+    attr_reader :response, :data
 
     def initialize(username)
       @response = Faraday.get("http://www.duolingo.com/users/#{username}")
+      @data = JSON.parse(response.body)
     end
 
     def total_points
-      info = JSON.parse(response.body)
-      info['languages'].inject(0) do |sum, lan|
+      data['languages'].inject(0) do |sum, lan|
         sum + lan["points"]
       end
     end
+
+    def languages_studied
+      languages = []
+      data['languages'].map do |lan|
+       languages << lan["language_string"] if lan["points"] > 0
+      end
+      languages
+    end
+
+    def current_languages
+      currently_studying = []
+      data['languages'].map do |lan|
+        currently_studying << lan['language_string'] if lan['current_learning']
+      end
+      currently_studying
+    end
+
   end
 end
--- spec/lib/duolingo/user_spec.rb
+++ spec/lib/duolingo/user_spec.rb
@@ -2,12 +2,27 @@ require_relative '../../spec_helper'
 require './lib/duolingo/user'
 
 describe "User" do
-  it "can get the important data" do
+  it "returns the  total points for all languages" do
     VCR.use_cassette('user') do
       username = "Novohispano"
       user = Duolingo::User.new(username)
-      user.total_points
       expect(user.total_points).to eq(2285)
+    end
+  end
+
+  it "all the studied languages" do
+    VCR.use_cassette('user') do
+      username = "Novohispano"
+      user = Duolingo::User.new(username)
+      expect(user.languages_studied).to eq(["Portuguese"])
+    end
+  end
+
+  it "languages that are currently being studied" do
+    VCR.use_cassette('user') do
+      username = "Novohispano"
+      user = Duolingo::User.new(username)
+      expect(user.current_languages).to eq(['Portuguese'])
     end
   end
 end
